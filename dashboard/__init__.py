c22d644b1b3a05f4ea2011b3e3318378bde6b5cc
Andrii Pastushenko <a.pastushenko@gsi.de>
2026-09-10 15:42:48 +0200

    exposed api

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,2 +1,14 @@
 from dashboard.dashboard import Dashboard
 from dashboard.dummy_generator import DummyGenerator
+
+from dashboard.models import DataField, InfoField, LoadedFile, FileSelection, Ratio
+
+__all__ = [
+	"Dashboard",
+	"DummyGenerator",
+	"DataField",
+	"InfoField",
+	"LoadedFile",
+	"FileSelection",
+	"Ratio",
+]
